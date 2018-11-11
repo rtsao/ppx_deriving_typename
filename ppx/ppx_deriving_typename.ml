ffb6621ee864ff48075b6b784e71611c6ceccf58
Ryan Tsao <ryan.j.tsao@gmail.com>
2018-11-11 08:42:35 -0800

    Migrate to ppxlib

--- ppx/ppx_deriving_typename.ml
+++ ppx/ppx_deriving_typename.ml
@@ -1,7 +1,7 @@
-module PC   = Ppx_core
-module PTC  = Ppx_type_conv.Type_conv
-module A    = PC.Ast_builder.Default
-module G    = PTC.Generator
+module PL   = Ppxlib
+module A    = PL.Ast_builder.Default
+module D    = PL.Deriving
+module G    = D.Generator
 module P    = Printf
 
 (*
@@ -10,24 +10,24 @@ module P    = Printf
  *  let foo_name = "foo"
  *  Where foo is the name of the type.
  *)
-let derive_typename ~(loc : PC.location)
+let derive_typename ~(loc : PL.location)
                     ~(path : string)
-                    ((_ : PC.rec_flag), (tdls : PC.type_declaration list)) 
-                    : PC.structure =
+                    ((_ : PL.rec_flag), (tdls : PL.type_declaration list))
+                    : PL.structure =
   ignore path;
-  let open PC in
-  let from_type_decl (td : PC.type_declaration) =
+  let open PL in
+  let from_type_decl (td : PL.type_declaration) =
     let name = P.sprintf "%s_name" td.ptype_name.txt in
     let binding = A.pvar ~loc name in
     let btype = [%type: string] in
     let pat = A.ppat_constraint ~loc binding btype in
     let expr = A.estring ~loc (td.ptype_name.txt) in
     let vb = A.value_binding ~loc ~pat ~expr in
-    A.pstr_value ~loc PC.Nonrecursive [vb]
+    A.pstr_value ~loc PL.Nonrecursive [vb]
   in
-  List.map ~f:from_type_decl tdls
+  List.map from_type_decl tdls
 
 (* Register *)
-let () = 
+let () =
   let str_type_decl = G.make_noarg derive_typename in
-  PTC.ignore (PTC.add "typename" ~str_type_decl)
+  D.ignore (D.add "typename" ~str_type_decl)
